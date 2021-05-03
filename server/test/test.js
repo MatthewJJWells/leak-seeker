3f55fb1985efd7c19de7d0e9d3b68b747c07babe
Tanja <tanjamiskovic1@gmail.com>
2021-05-03 18:31:27 +0100

    create mock data

--- server/test/test.js
+++ server/test/test.js
@@ -21,3 +21,38 @@ async function resetTestDatabase () {
   await mongooseRegModel.deleteMany({}, () => {});
   await mongooseVehicleModel.deleteMany({}, () => {});
 }
+
+const mockData = [
+  {
+    reg: 'reg1',
+    make: 'make1',
+    model: 'model1',
+    faults: [
+      {
+        summary: 'summary1',
+        description: 'description1',
+        year: 1,
+        area: 'area1',
+        priceToFix: 1,
+        faultLogged: new Date(),
+        rating: 1
+      }
+    ]
+  },
+  {
+    reg: 'reg2',
+    make: 'make2',
+    model: 'model2',
+    faults: [
+      {
+        summary: 'summary2',
+        description: 'description2',
+        year: 2,
+        area: 'area2',
+        priceToFix: 2,
+        faultLogged: new Date(),
+        rating: 2
+      }
+    ]
+  }
+];
