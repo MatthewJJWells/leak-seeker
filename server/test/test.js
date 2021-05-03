94c0b7b81c8f1e808cfd3012bf51dc98e0419ab5
Tanja <tanjamiskovic1@gmail.com>
2021-05-03 18:29:35 +0100

    initialize ./server/test/test.js file

---
+++ server/test/test.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+const assert = require('assert');
+const request = require('supertest');
+const app = require('../index');
+
+const { mongooseRegModel, mongooseVehicleModel } = require('../mongoose-schema');
+const { checkIfVehicleExists, checkIfRegExists } = require('../helpers');
+
+/*
+endpoints and matching functions
+POST /addfault -> addFault()
+GET /search/:reg -> getFaultsFormReg()
+GET /getallfaults -> getFunction()
+
+helper functions
+checkIfVehicleExists()
+checkIfRegExists()
+*/
