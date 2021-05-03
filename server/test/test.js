c4830d0fdd6a20bf51723c28dd3af9e7a2a54579
Tanja <tanjamiskovic1@gmail.com>
2021-05-03 18:32:53 +0100

    write tests for /addfault POST endpoint

--- server/test/test.js
+++ server/test/test.js
@@ -86,3 +86,55 @@ describe('/getallfaults GET endpoint', () => {
 
   afterEach(resetTestDatabase);
 });
+
+describe('/addfault POST endpoint', () => {
+  it('Post new data', async () => {
+    const response1 = await request(app)
+      .post('/addfault')
+      .send(mockData[0]);
+    assert.strictEqual(response1.statusCode, 200);
+    const response2 = await request(app)
+      .post('/addfault')
+      .send(mockData[1]);
+    assert.strictEqual(response2.statusCode, 200);
+
+    const response3 = await request(app).get('/getallfaults');
+    const data = JSON.parse(response3.text);
+    assert.strictEqual(data[0].make, mockData[0].make);
+    assert.strictEqual(data[0].model, mockData[0].model);
+    assert.strictEqual(data[0].faults[0].summary, mockData[0].faults[0].summary);
+    assert.strictEqual(data[1].make, mockData[1].make);
+    assert.strictEqual(data[1].model, mockData[1].model);
+    assert.strictEqual(data[1].faults[0].summary, mockData[1].faults[0].summary);
+  });
+
+  it('Post new data to an already tracked vehicle', async () => {//!
+    const response1 = await request(app)
+      .post('/addfault')
+      .send(mockData[0]);
+    assert.strictEqual(response1.statusCode, 200);
+    const response2 = await request(app)
+      .post('/addfault')
+      .send(mockData[0]);
+    assert.strictEqual(response2.statusCode, 200);
+
+    const response3 = await request(app).get('/getallfaults');
+    const data = JSON.parse(response3.text);
+    assert.strictEqual(data[0].make, mockData[0].make);
+    assert.strictEqual(data[0].model, mockData[0].model);
+    assert.strictEqual(data[0].faults[0].summary, mockData[0].faults[0].summary);
+    assert.strictEqual(data[0].faults[1].summary, mockData[0].faults[0].summary);
+  });
+
+  it('Respond with 400 status code on bad request', async () => {
+    const response1 = await request(app)
+      .post('/addfault')
+      .send({ data: 'invalid' });
+    assert.strictEqual(response1.statusCode, 400);
+    const response2 = await request(app).get('/getallfaults');
+    const data = JSON.parse(response2.text);
+    assert.strictEqual(data.length, 0);
+  })
+
+  afterEach(resetTestDatabase);
+});
