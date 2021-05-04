8d2d342fb6d6343b6078ddd32595eb6e6890306c
Tanja <tanjamiskovic1@gmail.com>
2021-05-04 10:40:29 +0100

    clean up unnecessary comments

--- server/router.js
+++ server/router.js
@@ -1,23 +1,12 @@
-
 const controller = require('./controller.js');
 const router = require('express').Router();
 
-// GET REQUEST
 // GET ALL - FOR TESTING
 router.get('/getallfaults', (req, res) => controller.getFunction(req, res));
 
 // GET SPECIFIC FAULTS
 router.get('/search/:reg', (req, res) => controller.getFaultsFromReg(req, res));
 
-// // POST REQUEST
 router.post('/addfault', (req, res) => controller.addFault(req, res));
-
-// // PUT (UPDATE) REQUEST
-// router.put('/:name/:newValue', (req, res) => controller.updateFunction(req, res));
-// router.patch()
-
-// // DELETE REQUEST
-// router.delete('/:name', (req, res) => controller.deleteFunction(req, res));
-
 
 module.exports = router;
